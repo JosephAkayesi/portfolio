d876934f68b015a404f772686827e4ea10429770
Joseph Akayesi <josephakayesi@gmail.com>
2019-05-18 23:34:43 +0000

    Add path module to server.js to resolve app crash in heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 
 // Cors middleware
 app.use(cors())
