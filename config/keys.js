0909bdd53aeb6e4b83722fe46ce964991eb5d4cc
Joseph Akayesi <josephakayesi@gmail.com>
2019-05-18 22:42:29 +0000

    Removed config/keys.js file to allow it to be pushed to remote repo. This will enable CI/CD piepleine to function

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+if(process.env.NODE_ENV){
+    module.exports = {
+        mongoURI: process.env.MONGOURI
+    }
+}
+else {
+    module.exports = {
+        mongoURI:  'mongodb://localhost/portfolio',
+    }
+}
