a85214ee31067ae987e39c4832a30a069e1c2feb
Joseph Akayesi <josephakayesi@gmail.com>
2019-05-19 21:43:00 +0000

    Make setBio function async so setTimeout calls when it resolves.

--- client/src/actions/bioActions.js
+++ client/src/actions/bioActions.js
@@ -2,8 +2,8 @@ import { SET_BIO } from './types'
 import axios from 'axios'
 
 // Set bio
-export const setBio = () => dispatch => {
-    axios.get('/api/bio')
+export const setBio = async () => dispatch => {
+    await axios.get('/api/bio')
         .then(res => dispatch({
             type: SET_BIO,
             payload: res.data[0]
