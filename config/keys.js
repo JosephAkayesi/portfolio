2335e12b1a2aecb914b3a581207da602fe0e3290
JosephAkayesi <35032334+JosephAkayesi@users.noreply.github.com>
2019-05-20 07:05:04 +0000

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -5,7 +5,6 @@ if(process.env.NODE_ENV){
 }
 else {
     module.exports = {
-        // mongoURI:  'mongodb://localhost/portfolio',
-        mongoURI:  'mongodb://joseph:joseph94@ds147946.mlab.com:47946/portfolio',
+        mongoURI:  'mongodb://localhost/portfolio',
     }
 }
