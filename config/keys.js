8cbd4cf5c898fa163fb1d61f1be87ad826ee983e
Joseph Akayesi <josephakayesi@gmail.com>
2019-07-29 20:43:57 +0000

    Add prerender to server and include meta tags to improve SEO

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,12 @@
 if(process.env.NODE_ENV){
     module.exports = {
-        mongoURI: process.env.MONGOURI
+        mongoURI: process.env.MONGOURI,
+        prerenderToken: process.env.PRERENDERTOKEN
     }
 }
 else {
     module.exports = {
         mongoURI:  'mongodb://localhost/portfolio',
+        prerenderToken: process.env.PRERENDERTOKEN
     }
 }
